26fa1323db04696b31b3bf429587abf06cfd5c6b
DzenisKajevic <dzenis.kajevic24@gmail.com>
2025-05-17 20:17:03 +0200

    Fixed equipment creation

--- src/main/java/com/NBP/NBP/services/EquipmentService.java
+++ src/main/java/com/NBP/NBP/services/EquipmentService.java
@@ -1,11 +1,17 @@
 package com.NBP.NBP.services;
 
+import com.NBP.NBP.models.Category;
 import com.NBP.NBP.models.Equipment;
+import com.NBP.NBP.models.Laboratory;
 import com.NBP.NBP.models.dtos.EquipmentWithDetailsDTO;
 import com.NBP.NBP.models.dtos.PaginatedEquipmentResponseDTO;
 import com.NBP.NBP.models.enums.EquipmentStatus;
+import com.NBP.NBP.repositories.CategoryRepository;
 import com.NBP.NBP.repositories.EquipmentRepository;
+import com.NBP.NBP.repositories.LaboratoryRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,10 +20,17 @@ import java.util.Optional;
 @Service
 public class EquipmentService {
     private final EquipmentRepository equipmentRepository;
+    private final CategoryRepository categoryRepository;
+    private final LaboratoryRepository laboratoryRepository;
 
     @Autowired
-    public EquipmentService(EquipmentRepository equipmentRepository) {
+    public EquipmentService(
+            EquipmentRepository equipmentRepository,
+            CategoryRepository categoryRepository,
+            LaboratoryRepository laboratoryRepository) {
         this.equipmentRepository = equipmentRepository;
+        this.categoryRepository = categoryRepository;
+        this.laboratoryRepository = laboratoryRepository;
     }
 
     public List<Equipment> getAllEquipment() {
@@ -56,7 +69,39 @@ public class EquipmentService {
     }
 
     public int saveEquipmentWithDTO(EquipmentWithDetailsDTO equipment) {
-        return equipmentRepository.save(equipment.toEquipment());
+        if (equipment.getLaboratoryName() == null && EquipmentStatus.LABORATORY.equals(equipment.getStatus())) {
+            throw new IllegalArgumentException("Cannot set status to LABORATORY without providing laboratoryName.");
+        }
+
+        if (equipment.getLaboratoryName() != null) {
+            Optional<Laboratory> labOpt = laboratoryRepository.findByName(equipment.getLaboratoryName());
+
+            if (labOpt.isEmpty()) {
+                throw new IllegalArgumentException(
+                        "Laboratory with name '" + equipment.getLaboratoryName() + "' not found.");
+            }
+
+            equipment.setLaboratoryId(labOpt.get().getId());
+        }
+
+        if (equipment.getCategoryName() != null) {
+            Optional<Category> categoryOpt = categoryRepository.findByName(equipment.getCategoryName());
+            if (categoryOpt.isEmpty()) {
+                throw new IllegalArgumentException(
+                        "Category with name '" + equipment.getCategoryName() + "' not found.");
+            }
+            equipment.setCategoryId(categoryOpt.get().getId());
+        }
+
+        if (equipment.getCategoryId() == null) {
+            throw new IllegalArgumentException("categoryId must be set either directly or via categoryName.");
+        }
+        try {
+            return equipmentRepository.save(equipment.toEquipment());
+        } catch (DuplicateKeyException e) {
+            throw new IllegalArgumentException(
+                    "An equipment with the name '" + equipment.getName() + "' already exists.");
+        }
     }
 
     public int updateEquipment(EquipmentWithDetailsDTO equipment) {
