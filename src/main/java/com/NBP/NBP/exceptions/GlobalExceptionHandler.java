e6de843073c212dd02ee0460a059b049705e4358
DzenisKajevic <dzenis.kajevic24@gmail.com>
2025-05-17 13:50:15 +0200

    Adjusted GlobalExceptionHandler (string to json, added 40x handler)

--- src/main/java/com/NBP/NBP/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/NBP/NBP/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.NBP.NBP.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDenied(AccessDeniedException e) {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(Map.of("error", "Access denied"));
+    }
 }
